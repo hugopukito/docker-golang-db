9301b500da53a7fd0ffd1f0ff4bb2d6e20a94c81
hugopukito <gogo26hugop@gmail.com>
2023-02-05 02:54:54 +0100

    go app waiting for services to be ready, add persistency with volumes

--- README.md
+++ README.md
@@ -7,3 +7,23 @@ Simple Go app running a server, initiating connection with mysql, rabbitmq and r
 Using DockerFile to compile Go app
 
 Using docker-compose.yml for three databases and go app service 
+
+## Prerequisites
+
+Install docker, docker compose plugin
+
+### Persistency
+
+Create directories for volumes: 
+
+```bash
+mkdir ~/devops/data/mysql
+mkdir ~/devops/data/redis
+mkdir ~/devops/data/rabbitmq
+```
+
+## Usage
+
+```bash
+docker compose up
+```
--- main.go
+++ main.go
@@ -12,37 +12,41 @@ import (
 )
 
 var err error
+var db *sql.DB
 
 func main() {
-	_, err = sql.Open("mysql", "user1:user1@tcp(127.0.0.1:3307)/docker_db")
+	db, err = sql.Open("mysql", "user1:user1@tcp(mysql:3306)/docker_db")
 	if err != nil {
 		log.Println("error mysql connection init" + err.Error())
-	} else {
-		log.Println("mysql connection initiated")
 	}
 
-	opt, err := redis.ParseURL("redis://127.0.0.1:6380")
+	opt, err := redis.ParseURL("redis://redis:6379")
 	if err != nil {
 		log.Println("error connection redis parse")
 	}
 	redisClient := redis.NewClient(opt)
-	if err := redisClient.Ping(); err != nil {
-		log.Println("error redis connection init" + err.String())
-	} else {
-		log.Println("redis connection initiated")
+	if err := redisClient.Ping(); err.String() != "ping: PONG" {
+		log.Println("error redis connection init: " + err.String())
 	}
 
-	_, err = amqp.Dial("amqp://guest:guest@127.0.0.1:5673/")
+	_, err = amqp.Dial("amqp://guest:guest@rabbitmq:5672")
 	if err != nil {
 		log.Println("error rabbitmq connection init" + err.Error())
-	} else {
-		log.Println("rabbitmq connection init")
 	}
 
 	http.HandleFunc("/", HelloServer)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/test-sql", TestSQL)
+	http.ListenAndServe(":8095", nil)
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, ni****")
+}
+
+func TestSQL(w http.ResponseWriter, r *http.Request) {
+	db.Exec("CREATE TABLE IF NOT EXISTS containers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)")
+
+	db.Exec("INSERT INTO containers (name) VALUE ('Armand')")
+
+	fmt.Fprintf(w, "sql")
 }
